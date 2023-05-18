6913a724d46fa3feb98ef0525043a322340fd4a3
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2023-05-18 16:42:50 -0500

    Fixed solver implementation then switched to hull plus dotproduct impolementation

--- code/polytope.py
+++ code/polytope.py
@@ -1,8 +1,10 @@
 from mip import Model, xsum, minimize
 import mip
+from scipy.spatial import ConvexHull
+from sklearn.decomposition import PCA
 import numpy as np
 
-def is_inside(polytope, point, delta=0):
+def is_inside_lp(archetypes, point, delta=0):
     """
     polytope: numpy array of shape (n,m) where n is the number of dimensions of the space and m is the number of vertices in the polytope
     point: numpy array of shape (n,) where n is the number of dimensions of the space
@@ -11,9 +13,9 @@ def is_inside(polytope, point, delta=0):
 
     model = Model()
     model.verbose = 0
-    x = [model.add_var(name='x'+str(i)) for i in range(polytope.shape[1])]
-    for i in range(polytope.shape[0]):
-        model += xsum(polytope[i,j]*x[j] for j in range(polytope.shape[1])) == point[i]
+    x = [model.add_var(name='x'+str(i)) for i in range(archetypes.shape[1])]
+    for i in range(archetypes.shape[0]):
+        model += xsum(archetypes[i,j]*x[j] for j in range(archetypes.shape[1])) == point[i]
     model += xsum(x) >= 1
     model += xsum(x) <= 1+delta
     
@@ -24,3 +26,13 @@ def is_inside(polytope, point, delta=0):
     # print(model.status)
     return model.status == mip.OptimizationStatus.OPTIMAL
 
+
+def is_inside(polytope, point):
+    # Compute the convex hull of the principal components
+    hull = ConvexHull(polytope)
+
+    # Check if the projected point is inside the convex hull of the principal components
+    for eq in hull.equations:
+        if np.dot(eq[:-1], point) + eq[-1] > 0:
+            return False
+    return True
