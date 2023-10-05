4edbd8feb68e02f7cce15683b489ef8b957a7319
Bryan Glazer <bryan.glazer@vanderbilt.edu>
2023-10-05 14:28:19 -0500

    Running fokker planck on POU5F1 data, fixing bug in Spxt loss

--- code/flow_model/fokker_planck.py
+++ code/flow_model/fokker_planck.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import matplotlib
 import matplotlib.pyplot as plt
-# import scanpy as sc
+import scanpy as sc
 from scipy.stats import rv_histogram
 from torch.nn import Linear, ReLU
 
@@ -31,14 +31,6 @@ class MLP(torch.nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
-
-#%%
-# genotype='wildtype'
-# dataset = 'net'
-# adata = sc.read_h5ad(f'../../data/{genotype}_{dataset}.h5ad')
-# data = adata.layers['raw']
-# X = data.toarray()
-device = 'cuda:0'
 
 #%%
 # This generates a scipy continuous distribution from a histogram
@@ -70,169 +62,203 @@ class Pxt(torch.nn.Module):
         self.model = MLP(2, 1, hidden_dim, n_layers).to(device)
         self.device = device
 
-    # Convert scalar t to a tensor of the same shape as x, for input to the model
-    def t_(self, t, shape):
-        return torch.ones(shape, device=self.device)*t
-    
-    def p(self, x, t):
-        t = self.t_(t, x.shape)
-        return torch.exp(self.model(torch.hstack((x, t))))
+    def p(self, x, ts):
+        xs = x.repeat((1,ts.shape[0])).T.unsqueeze(2)
+        tss = ts.repeat((x.shape[0],1)).T.unsqueeze(2).to(device)
+        xts = torch.concatenate((xs,tss), dim=2)
+        ps = torch.exp(pxt.model(xts))
+        return ps
 
     # Compute the probability density p(x,t) using the neural network
-    def forward(self, x, t):
-        p = self.p(x, t)
+    def forward(self, x, ts):
+        p = self.p(x, ts)
         return p
     
     # Compute the partial derivative of p(x,t) with respect to x
-    def dx(self, x, t, hx=1e-3):
-        xgrad = (self.p(x+hx, t) - self.p(x-hx, t))/(2*hx)
+    def dx(self, x, ts, hx=1e-3):
+        xgrad = (self.p(x+hx, ts) - self.p(x-hx, ts))/(2*hx)
         return xgrad
 
     # Compute the partial derivative of p(x,t) with respect to t
-    def dt(self, x, t, ht=1e-3):
-        tgrad = (self.p(x, t+ht) - self.p(x, t-ht))/(2*ht)
+    def dt(self, x, ts, ht=1e-3):
+        tgrad = (self.p(x, ts+ht) - self.p(x, ts-ht))/(2*ht)
         return tgrad
 
-    # Integral of (d/dx p(x,t)) over t
-    # TODO instead of using a for loop, should use a combined
-    # tensor across all timesteps. Not sure if this will 
-    # consume too much GPU memory though.
-    # xs = x.repeat((1,ts.shape[0])).T.unsqueeze(2)
-    # tss = ts.repeat((x.shape[0],1)).T.unsqueeze(2).to(device)
-    # xts = torch.concatenate((xs,tss), dim=2)
-    # ps = pxt.model(xts)
-    def Sdx_dt(self, x, hx=1e-3, steps=100):
-        ts = torch.linspace(0, 1, steps, device=self.device)
-        ht = ts[1] - ts[0]
-        sum = torch.zeros(x.shape, device=self.device, requires_grad=True)
-        for t in ts:
-            dx = self.dx(x, t, hx=hx)
-            sum = sum + (dx*ht)
-        return sum
+#%%
+device = 'cuda:0'
+#%%
+genotype='wildtype'
+dataset = 'net'
+adata = sc.read_h5ad(f'../../data/{genotype}_{dataset}.h5ad')
+# data = adata.layers['raw']
+#%%
+cell_types = adata.obs['cell_type']
+nmp_cells = cell_types[cell_types=='NMP']
+gene = 'POU5F1'
+geneX = adata[:,gene].X.toarray()
+X0 = geneX[cell_types=='NMP']
+X1 = geneX.copy()
 
 #%%
 # Generate simple test data
 # Initial distribution
-X0 = torch.randn((1000, 1), device=device)
-# Final distribution is initial distribution plus another normal shifted by +4
-X1 = torch.randn((1000, 1), device=device)+4
-X1 = torch.vstack((X0, X1))
+# X0 = torch.randn((1000, 1), device=device)
+# # Final distribution is initial distribution plus another normal shifted by +4
+# X1 = torch.randn((1000, 1), device=device)+4
+# X1 = torch.vstack((X0, X1))
 # Plot the two distributions
-_=plt.hist(X0.cpu().numpy(), bins=30, alpha=.3)
-_=plt.hist(X1.cpu().numpy(), bins=30, alpha=.3)
+_=plt.hist(X0, bins=30, alpha=.3)
+_=plt.hist(X1, bins=30, alpha=.3)
 #%%
+X0 = torch.tensor(X0, device=device, dtype=torch.float32, requires_grad=False)
+X1 = torch.tensor(X1, device=device, dtype=torch.float32, requires_grad=False)
 #%%
 epochs = 500
 steps = 1
 hx = 1e-3
 ht = 1e-3
 #%%
+ts = torch.linspace(0, 1, 100, device=device, requires_grad=False)
+# ## %%
 # Generate a continuous distribution from the data
 pD = gen_p(X1.cpu().numpy(), bins=100)
+pD0 = gen_p(X0.cpu().numpy(), bins=100)
+# Generate a set of points to evaluate the distribution
+span = X1.max() - X1.min()
+x0 = torch.linspace(X1.min()-span*.25, X1.max()+span*.25, 1000, device=device, requires_grad=False)[:,None]
+# x0 = torch.linspace(X1.min(), X1.max(), 1000, device=device, requires_grad=False)[:,None]
+pX_D0 = torch.tensor(pD0.pdf(x0.cpu().numpy()), device=device, dtype=torch.float32, requires_grad=False)
+z_pX_D0 = float(pX_D0.sum())
+pX = torch.tensor(pD.pdf(x0.cpu().numpy()), device=device, dtype=torch.float32, requires_grad=False)
+z_pX = float(pX.sum())
+# pX_D0 = pX_D0 / z_pX_D0
+# pX = pX / z_pX
+#%%
 # Initialize the neural networks
 pxt = Pxt(20, 3, device)
 ux = Ux(20, 3, device)
-pxt_optimizer = torch.optim.Adam(pxt.parameters(), lr=1e-4)
+# Initialize the optimizers
+pxt_optimizer = torch.optim.Adam(pxt.parameters(), lr=5e-4)
 ux_optimizer = torch.optim.Adam(ux.parameters(), lr=1e-3)
-pD0 = torch.tensor(pD.pdf(X0.cpu().numpy()), device=device, dtype=torch.float32, requires_grad=False)
-
-ts = torch.linspace(0, 1, 100, device=device, requires_grad=False)
-## %%
-# pxt0_optimizer = torch.optim.Adam(pxt.parameters(), lr=1e-3)
-# for i in range(epochs):
-#     pxt0_optimizer.zero_grad()
-#     l_pD0 = ((pxt(X0, t=0) - pD0)**2).mean()
-#     l_pD0.backward()
-#     # torch.nn.utils.clip_grad_norm_(pxt.parameters(), .001)
-#     pxt0_optimizer.step()
-#     print(f'{i} l_pD0={float(l_pD0.mean()):.5f}')
 #%%
-for i in range(epochs):
+# # This is a pre-training step to get p(x, t=0) to match the initial condition
+pxt0_optimizer = torch.optim.Adam(pxt.parameters(), lr=1e-2)
+zero = torch.zeros(1)
+for i in range(500):
+    pxt0_optimizer.zero_grad()
+    l_pD0 = ((pxt(x0, ts=zero) - pX_D0)**2).mean()
+    l_pD0.backward()
+    # torch.nn.utils.clip_grad_norm_(pxt.parameters(), .001)
+    pxt0_optimizer.step()
+    print(f'{i} l_pD0={float(l_pD0.mean()):.5f}')
+#%%
+ts = torch.linspace(0, 1, 100, device=device, requires_grad=False)
+ht = ts[1] - ts[0]
+
+l_fps = np.zeros(epochs)
+l_Spxts = np.zeros(epochs)
+l_p0s = np.zeros(epochs)
+for epoch in range(epochs):
+    # Sample from the data distribution
+    x = pD.rvs(size=1000)
+    px = torch.tensor(pD.pdf(x), device=device, dtype=torch.float32, requires_grad=False)
+    x = torch.tensor(x, device=device, dtype=torch.float32, requires_grad=False)[:,None]
+
     pxt_optimizer.zero_grad()
     ux_optimizer.zero_grad()
-    x = pD.rvs(size=(1000,1))
-    px = torch.tensor(pD.pdf(x), device=device, dtype=torch.float32, requires_grad=False)
-    x = torch.tensor(x, device=device, dtype=torch.float32, requires_grad=False)
-    ppx = (pxt(x, t=0) - pxt(x, t=1) - ux(x) * pxt.Sdx_dt(x)) / (ux.dx(x))
-    
-    l_px = ((ppx - px)**2).mean() #+ ((pxt(X0, t=0) - pD0)**2).mean()
-    l_px.backward()
-    torch.nn.utils.clip_grad_norm_(pxt.parameters(), .001)
-    pxt_optimizer.step()
+    # This is the initial condition p(x, t=0)=p_D0 
+    l_p0 = ((pxt(x0, ts=zero) - pX_D0)**2).mean()
+    l_p0.backward()
 
-    l_fp = torch.zeros(1, device=device, requires_grad=True)
-    up = lambda x, t: ux(x) * pxt(x, t)
-    for t in ts:
-        up_dx = -(up(x+hx,t) - up(x-hx,t))/(2*hx)
-        l_fp_t = (pxt.dt(x, t) - up_dx)**2
-        l_fp = l_fp + l_fp_t.mean()
-    l_fp = l_fp / len(ts)
+    # This is the marginal p(x) = int p(x,t) dt
+    Spxt = pxt(x, ts).sum(dim=0) * ht
+    # Ensure that the marginal p(x) matches the data distribution
+    l_Spxt = ((Spxt[:,0] - px)**2).mean()
+    l_Spxt.backward()
+
+    # This is the calculation of the term that ensures the
+    # derivatives match the Fokker-Planck equation
+    # d/dx p(x,t) = -d/dt (u(x) p(x,t))
+    up_dx = (ux(x+hx) * pxt(x+hx, ts) - ux(x-hx) * pxt(x-hx, ts))/(2*hx)
+    pxt_dts = pxt.dt(x, ts)
+    l_fp = ((pxt_dts + up_dx)**2).mean()
+
     l_fp.backward()
 
-    torch.nn.utils.clip_grad_norm_(ux.parameters(), .001)
+    pxt_optimizer.step()
     ux_optimizer.step()
 
-    # print(f'pxt0={float(pxt(x, t=0).mean()):.5f}, pxt1={float(pxt(x, t=1).mean()):.5f}')
-    # print(f'ux={float(ux(x).mean()):.5f}')
-    # print(f'Spxt_dx_dt={float(Spxt_dx_dt(pxt, x).mean()):.5f}')
-    # print(f'ux.dx={float(ux.dx(x).mean()):.5f}')
-    # print(f'pxt.dx={float(pxt.dx(x, t=0).mean()):.5f}')
-    # print(f'pxt.dt={float(pxt.dt(x, t=0).mean()):.5f}')
-    # print(f'ppx={float(ppx.mean()):.5f}')
-    print(f'{i} l_fp={float(l_fp.mean()):.5f}, l_px={float(l_px.mean()):.5f}')
-    # print('---')
-
+    print(f'{epoch} l_fp={float(l_fp.mean()):.5f}, l_px={float(l_Spxt.mean()):.5f}, l_p0={float(l_p0.mean()):.5f}')
+    l_fps[epoch] = float(l_fp.mean())
+    l_Spxts[epoch] = float(l_Spxt.mean())
+    l_p0s[epoch] = float(l_p0.mean())   
 #%%
-xs = torch.arange(X1.min(), X1.max(), .01, device=device)[:,None]
-ppxs = (pxt(xs, t=0) - pxt(xs, t=1) - ux(xs) * pxt.Sdx_dt(xs)) / (ux.dx(xs))
-px = torch.tensor(pD.pdf(xs.detach().cpu().numpy()), device=device, dtype=torch.float32, requires_grad=False)
-plt.plot(xs.detach().cpu().numpy(), ppxs.cpu().detach().numpy(), label='Model')
-plt.plot(xs.detach().cpu().numpy(), px.cpu().numpy(), label='Data')
+plt.title('Loss curves')
+plt.plot(l_fps[10:], label='l_fp')
+plt.plot(l_Spxts[10:], label='l_Spxt')
+plt.plot(l_p0s[10:], label='l_p0')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
 plt.legend()
 
 # %%
+# Plot the predicted p(x,t) at each timestep t
 colors = matplotlib.colormaps.get_cmap('viridis')
-xs = torch.arange(X1.min(), X1.max(), .01, device=device)[:,None]
-for t in torch.linspace(0, 1, 100, device=device):
-    # print(t)
-    pxst = pxt(xs, t)
-    plt.plot(xs.cpu().numpy(), pxst.cpu().detach().numpy(), c=colors(float(t)))
-    # plt.plot(xs.cpu().numpy(), pD.pdf(xs.cpu().numpy()), c='k', alpha=1)
-# plt.colorbar()
-#%%
-ts = torch.linspace(0, 1, 100, device=device)
-ppx = torch.zeros_like(xs)
-for t in ts:
-    pxst = pxt(xs, t)
-    ppx = ppx + pxst/len(ts)
-plt.plot(xs.cpu().numpy(), ppx.cpu().detach().numpy())
-plt.plot(xs.cpu().numpy(), pD.pdf(xs.cpu().numpy()), c='k', alpha=1)
-    
-#%%
-# Plot the cdf of the data and the final distribution
-# plt.plot(X1.cpu().numpy(), px_cdf.cpu().numpy(), label='Data CDF')
-# ppx_cdf = ppx.clip(0).cumsum(dim=0)/ppx.clip(0).sum()
-# plt.plot(X1.cpu().numpy(), ppx_cdf.detach().cpu().numpy(), label='Model CDF')
-# plt.legend()
-# %%
-xs = torch.arange(X1.min(), X1.max(), .1, device=device)[:,None]
-ts = torch.linspace(0, 1, 100, device=device)
-pxst = torch.zeros(xs.shape[0], ts.shape[0], device=device)
-for i,t in enumerate(ts):
-    pxst[:,i] = pxt(xs, t).squeeze()
+xs = x0
 
-# Cumulative mean of p(x,t) at each timestep t
-cum_pxst = pxst.cumsum(dim=1) / torch.arange(1, ts.shape[0]+1, device=device)[None,:]
-plt.imshow(cum_pxst.cpu().detach().numpy(), aspect='auto', interpolation='none', cmap='viridis')
+pxts = pxt(xs, ts).squeeze().T.cpu().detach().numpy()
+uxs = ux(xs).squeeze().cpu().detach().numpy()
+xs = xs.squeeze().cpu().detach().numpy()
+
+plt.title('p(x,t)')
+plt.plot(xs,  pD.pdf(xs), c='k', alpha=1)
+plt.plot(xs, pD0.pdf(xs), c='k', alpha=1)
+for i in range(0, ts.shape[0], 10):
+    t = ts[i]
+    plt.plot(xs, pxts[:,i], c=colors(float(t)))
+plt.xlabel('x')
+plt.ylabel('p(x,t)')
+# Add a colorbar to show the timestep
+sm = plt.cm.ScalarMappable(cmap=colors, norm=plt.Normalize(vmin=0, vmax=1))
+sm.set_array([])
+plt.colorbar(sm, label='timestep - t')
+
+# %%
+# This plots the cumulative mean of p(x,t) at each timestep t, going from t=0 (left) to t=1 (right)
+# Higher probability is shown in yellow, lower probability is shown in blue
+plt.title('Cumulative mean of p(x,t)')
+cum_pxt = pxts.cumsum(axis=1) / np.arange(1, ts.shape[0]+1)
+plt.imshow(cum_pxt, aspect='auto', interpolation='none', cmap='viridis')
+plt.ylabel('x')
+plt.xlabel('timestep - t')
+plt.colorbar()
+# %%
+# This plots the error of the cumulative mean of p(x,t) at each timestep t, going from t=0 (left) to t=1 (right)
+pxs = pD.pdf(xs)
+plt.title('Error of cumulative mean of p(x,t)')
+plt.imshow(pxs[:,None] - cum_pxt, aspect='auto', interpolation='none', cmap='RdBu')
+plt.ylabel('x')
+plt.xlabel('timestep - t')
 plt.colorbar()
 
 #%%
-plt.imshow(pxst.cpu().detach().numpy(), aspect='auto', interpolation='none', cmap='viridis')
+# This is the individual p(x,t) at each timestep t, going from t=0 (left) to t=1 (right)
+plt.title('p(x,t) at each timestep t')
+plt.imshow(pxts, aspect='auto', interpolation='none', cmap='viridis')
+plt.ylabel('x')
+plt.xlabel('timestep - t')
 plt.colorbar()
-
 # %%
-# for x in xs:
-#     print(float(ux(x)))
-# %%
-plt.plot(xs.cpu().detach().numpy(), ux(xs).cpu().detach().numpy())
+# Plot the u(x) term for all x
+fig, ax1 = plt.subplots(1,1, figsize=(10,5))
+plt.title('u(x) vs p(x)')
+ax1.plot(xs, -uxs, label='u(x)')
+# Add vertical and horizontal grid lines
+ax1.grid()
+ax1.set_ylabel('u(x)')
+ax1.set_xlabel('x')
+ax2 = ax1.twinx()
+ax2.plot(xs, pD.pdf(xs), c='k', alpha=1, label='p(x)')
+ax2.set_ylabel('p(x)')
+fig.legend()
 # %%
